ed3d245e24eeb91c1940f185629158d085047de9
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-13 22:44:58 +0600

    add trees

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,15 @@ import './Main.css';
 const Main = () => {
     return(
         <div className="main-container">
+            <div className='main-side'>
+                <img src={require('../../media/manytree.jpg')} alt="tree"/>
+            </div>
             <div className='main-content'>
                <NewsSlider style={{height:'50%', width:'100%'}}/>
+               
+            </div>
+            <div className='main-side' style={{alignItems:'flex-end'}}>
+                <img src={require('../../media/onetree.jpg')} alt="tree"/>
             </div>
         </div>
     )
